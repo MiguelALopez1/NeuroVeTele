792ba995301b14dea0465b5167658991e5cc7a1a
Veno59Moth <76063290+Venomoth8@users.noreply.github.com>
2024-04-01 14:00:40 -0400

    Delete pet_neurolocalization/general_page_model.dart

--- pet_neurolocalization/general_page_model.dart
+++
@@ -1,7 +0,0 @@
-class GeneralPageModel {
-  List<String> selectedBehaviorOptions = [];
-  List<String> selectedMentationOptions = [];
-  List<String> selectedPostureOptions = [];
-  List<String> selectedGaitOptions = [];
-  List<String> selectedInvoluntaryMovementsOptions = [];
-}
